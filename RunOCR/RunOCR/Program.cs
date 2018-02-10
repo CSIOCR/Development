9dce4a4b567d4d7296834dccf01f01e1528b289c
DJ <csiocr@163.com>
2018-02-11 02:03:12 +0800

    Finish step 1, 3, 4, remove OCREngines.cs

--- RunOCR/RunOCR/Program.cs
+++ RunOCR/RunOCR/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 
 namespace RunOCR
 {
@@ -14,13 +15,15 @@ namespace RunOCR
         // All of the files in the input parameters, the first parameter
         static List<string> dirs = new List<string>();
 
+        static List<string> newDir = new List<string>();
+
         static void Main(string[] args)
         {
             //1. parse parameters
-            //The first parameter - a picture file or a folder that contains picture files
+            //The first parameter - a folder that contains picture files
             //The second parameter - output path of analytic results, a folder
             //The third parameter - "-r" or "-R" indicate that will query each folder recursively inside if the first parameter is a folder
-            
+
             // determine the number of the parameters
             if (args.Length == 2)
             {
@@ -28,13 +31,9 @@ namespace RunOCR
                 {
                     dirs.AddRange(Directory.GetFiles(args[0]));
                 }
-                else if (File.Exists(args[0]))
-                {
-                    dirs.Add(args[0]);
-                }
                 else
                 {
-                    Console.WriteLine("{0} is not a valid file or directory.", args[0]);
+                    Console.WriteLine("{0} is not a valid directory.", args[0]);
 
                     return;
                 }
@@ -43,15 +42,11 @@ namespace RunOCR
             {
                 if (Directory.Exists(args[0]) & (args[2] == @"-r" || args[2] == @"-R"))
                 {
-                    ProcessDirectory(args[0]);
-                }
-                else if (File.Exists(args[0]))
-                {
-                    dirs.Add(args[0]);
+                    dirs = ProcessDirectory(args[0]);
                 }
                 else
                 {
-                    Console.WriteLine("{0} is not a valid file or directory.", args[0]);
+                    Console.WriteLine("{0} is not a valid directory.", args[0]);
 
                     return;
                 }
@@ -65,21 +60,28 @@ namespace RunOCR
 
             if (!Directory.Exists(args[1]))
             {
-                Console.WriteLine("{0} is not a valid file or directory.", args[1]);
+                Console.WriteLine("{0} is not a valid directory.", args[1]);
 
                 return;
             }
 
+
             //2. optimize and process noise points  --  Yao Di
 
             //3. will .jpg convert to .tif format
+            RunPictureConverter(args[0], PicFormat.JPG, PicFormat.TIF);
 
             //4. retrieve picture files and call tesseract to generate box result files
             //   1) retrieve all picture(.tif) files according to the first parameter and the third parameter
             //   2) by checking config file to read the path of tesseract(no need, manually add the path of tesseract into environment variables), then call it 
-            List<string> TifPicture = OCREngines.FilterPicture(dirs);
 
-            OCREngines.RunTesseract(TifPicture);
+            // retrieve the files of the target folder due to the second step changed the number of the files
+            newDir = ProcessDirectory(args[0]);
+
+            List<string> TifPicture = FilterPicture(newDir, PicFormat.TIF);
+
+
+            RunTesseract(TifPicture);
 
 
             //5. process noise point according to initializing result files
@@ -95,26 +97,110 @@ namespace RunOCR
             //   3) put this text file in the path of the second paramter that user specified
 
             //8. remove all temp files we generated from the directory user given the first parameter.
+
+
         }
 
         // Process all files in the directory passed in, recurse on any directories 
         // that are found, and process the files they contain.
-        public static void ProcessDirectory(string targetDirectory)
+        public static List<string> ProcessDirectory(string targetDirectory)
         {
+            List<string> files = new List<string>();
             // Process the list of files found in the directory.
             string[] fileEntries = Directory.GetFiles(targetDirectory);
             foreach (string fileName in fileEntries)
-                ProcessFile(fileName);
+                files.Add(fileName);
 
             // Recurse into subdirectories of this directory.
             string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
             foreach (string subdirectory in subdirectoryEntries)
                 ProcessDirectory(subdirectory);
+
+            return files;
         }
 
-        public static void ProcessFile(string path)
+        public static void RunTesseract(List<string> dirs)
         {
-            dirs.Add(path);
+            string cmdPath = "cmd.exe";
+
+            try
+            {
+                Parallel.ForEach(dirs, (dir) =>
+                {
+                    using (Process p = new Process())
+                    {
+                        p.StartInfo.FileName = cmdPath;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.StartInfo.CreateNoWindow = true;
+                        p.Start();
+
+                        string picName = dir.Substring(0, dir.Length - 4);
+                        string cmd = "tesseract " + dir + " " + picName + " -l num_All10000_Review+num_25+num_All10000_Updated batch.nochop makebox";
+                        p.StandardInput.WriteLine(cmd + "&exit");
+
+                        p.StandardInput.AutoFlush = true;
+
+                        string output = p.StandardOutput.ReadToEnd();
+                        Console.WriteLine(output);
+                        p.WaitForExit();
+                        p.Close();
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+
+        // filter out specified format files in the specified folder
+        public static List<string> FilterPicture(List<string> dirs, PicFormat format) => dirs.FindAll(delegate (string s)
+        {
+
+            switch (format)
+            {
+                case PicFormat.TIF:
+                    return s.ToLower().Contains(".tif");
+                case PicFormat.JPG:
+                    return s.ToLower().Contains(".jpg");
+                default:
+                    return false;
+            }
+
+        });
+
+        public static void RunPictureConverter(string folder, PicFormat oriFormat, PicFormat desFormat)
+        {
+            string cmdPath = "cmd.exe";
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = cmdPath;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = false;
+                p.Start();
+
+                string cmd = "PictureConverter.exe " + folder + " " + oriFormat + " " + desFormat;
+                p.StandardInput.WriteLine(cmd + "&exit");
+
+                p.StandardInput.AutoFlush = true;
+
+                string output = p.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+                p.WaitForExit();
+                p.Close();
+            }
+        }
+
+        public enum PicFormat
+        {
+            TIF,
+            JPG
         }
     }
 }
